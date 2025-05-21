145c991bad0d6f76b5a367e4f81120bf7287e81a
varunriyer <vs7835@srmist.edu.in>
2025-05-21 14:28:13 +0530

    Add mouse event handlers for click and leave actions on #click-see and #click-leave elements --> Learnt the working of mousedown and mouseup methods

--- hide_para.js
+++ hide_para.js
@@ -15,3 +15,15 @@ $(function () {
         alert("Who asked you to leave?")
     });
 });
+
+$(function () {
+    $("#click-see").mousedown(function () {
+        alert("Shouldn't have clicked");
+    });
+});
+
+$(function () {
+    $("#click-leave").mouseup(function () {
+        alert("Shouldn't have stopped clicking");
+    });
+});
